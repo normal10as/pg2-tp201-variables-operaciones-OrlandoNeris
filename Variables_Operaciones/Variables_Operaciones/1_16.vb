2ab13ed674dd3ea828c395768c19d00faed1ea64
OrlandoNeris <orlandoneris24@gmail.com>
2018-04-19 23:01:15 -0300

    Estructuras

---
+++ Variables_Operaciones/Variables_Operaciones/1_16.vb
@@ -0,0 +1,18 @@
+﻿Module _1_16
+
+    'Crear un programa que se ingrese una fecha y un número dados de días, Mostrar como
+    'resultado la fecha obtenida de sumar la cantidad de días a la fecha dada.
+
+    Sub main()
+
+        Dim fecha As Date = Today
+        Dim diasadicionados As Integer = 54
+
+        Console.WriteLine("La fecha ingresada es {0}", fecha)
+        Console.WriteLine("La Cantidad de dias a agregar son {0}", diasadicionados)
+        Console.WriteLine("La fecha final seria: {0}", fecha.AddDays(diasadicionados))
+
+        Console.ReadKey()
+
+    End Sub
+End Module
